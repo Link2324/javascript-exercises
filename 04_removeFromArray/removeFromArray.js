85f7305ade08b9a8595445774e1aa629ee722ba5
Mangal-Kushwah <63869813+Link2324@users.noreply.github.com>
2024-02-14 15:15:37 +0530

    Complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, item) {
+    const newArray = [];
+    const args = Array.from(arguments);
+    outer:
+    for (const el of array) {
+        for (let i = 1; i < args.length; i++) {
+            if (el === args[i]) {
+                continue outer;
+            }
+        }
+        newArray.push(el);
+    }
 
+    return newArray;
 };
 
+console.log(removeFromArray([12, 12, 12], 2 ,3 ,4 ,4))
 // Do not edit below this line
 module.exports = removeFromArray;
