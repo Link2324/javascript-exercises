6fc81043ad8744e5a3fbd0403a1d751871927a54
Mangal-Kushwah <63869813+Link2324@users.noreply.github.com>
2024-02-14 16:01:49 +0530

    Complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+    if (num1 < 0 || num2 < 0 || typeof num1 !== 'number' || typeof num2 !== 'number') {
+        return "ERROR";
+    }
 
+    let sum = 0;
+    let larger = (num1 > num2) ? num1 : num2;
+    let smaller = (num1 < num2) ? num1 : num2;
+
+    for (let i = smaller; i <= larger; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
