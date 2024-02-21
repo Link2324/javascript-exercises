8829fd54f3da9ccd401070a41fb13775b6ea1740
Mangal-Kushwah <63869813+Link2324@users.noreply.github.com>
2024-02-21 20:21:42 +0530

    Complete 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    const year = new Date().getFullYear()  
+    return arr.reduce((oldest, current) => {
+        let currentYear, oldestYear;
+        if (current.yearOfDeath === undefined) {
+            currentYear = year - current.yearOfBirth;
+        } else {
+            currentYear = current.yearOfDeath - current.yearOfBirth;
+        }
 
+        if (oldest.yearOfDeath === undefined) {
+            oldestYear = year - oldest.yearOfBirth;
+        } else {
+            oldestYear = oldest.yearOfDeath - oldest.yearOfBirth;
+        }
+
+        return oldestYear > currentYear ? oldest: current;
+    })
 };
 
 // Do not edit below this line
