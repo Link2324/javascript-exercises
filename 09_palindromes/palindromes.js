9fef969812b8cf7d17cf10689037333da2d9eee8
Mangal-Kushwah <63869813+Link2324@users.noreply.github.com>
2024-02-21 19:36:37 +0530

    Complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const arr = str.toLowerCase().split('')
+            .filter((item) => {
+                let regex = /^[a-z0-9]+$/;
+                return regex.test(item)
+            })
 
+    let reverse = arr.slice().reverse();
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] !== reverse[i]) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
